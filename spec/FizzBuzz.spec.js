09e067019da4573fc04059e11d731210d7f66756
rachdi <bigking3100@gmail.com>
2017-01-20 11:17:35 +0100

    change valeur divible par 5

--- spec/FizzBuzz.spec.js
+++ spec/FizzBuzz.spec.js
@@ -3,7 +3,7 @@ var FizzBuzzOf = require('../lib/fizzBuzz').FizzBuzzOf
 describe("FizzBuzzOf execution", function () {
 
 	it("should return 1 with parameter 1", function () {
-		expect(FizzBuzzOf(1)).toBe("1");
+		expect(FizzBuzzOf(1)).toBe("5");
 	});
 
 });
