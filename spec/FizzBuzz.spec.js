20cf78b10dcf219e2ef3d0f9f44441199c41bd56
rachdi <bigking3100@gmail.com>
2017-01-20 12:10:20 +0100

    return fizz %3 ok

--- spec/FizzBuzz.spec.js
+++ spec/FizzBuzz.spec.js
@@ -3,7 +3,14 @@ var FizzBuzzOf = require('../lib/fizzBuzz').FizzBuzzOf
 describe("FizzBuzzOf execution", function () {
 
 	it("should return 1 with parameter 1", function () {
-		expect(FizzBuzzOf(1)).toBe("5");
+		expect(FizzBuzzOf(1)).toBe(1);
 	});
 
+	it("should return 2 with parameter 2", function(){
+		expect(FizzBuzzOf(2)).toBe(2);
+	})
+
+	it("should return Fizz with parameter 3", function(){
+		expect(FizzBuzzOf(3)).toBe("Fizz");
+	})
 });
